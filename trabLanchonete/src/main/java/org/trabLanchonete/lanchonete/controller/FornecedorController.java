5a4133029cf4d739d49703dc8328901b42c6ce33
Rebeca <rebecarodriguessantos@gmail.com>
2021-02-04 02:02:46 -0300

    add controller

--- trabLanchonete/src/main/java/org/trabLanchonete/lanchonete/controller/FornecedorController.java
+++ trabLanchonete/src/main/java/org/trabLanchonete/lanchonete/controller/FornecedorController.java
@@ -1,4 +1,26 @@
+package org.trabLanchonete.lanchonete.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.trabLanchonete.lanchonete.model.Fornecedor;
+import org.trabLanchonete.lanchonete.repositorio.FornecedorRepositorio;
+
+@Controller
 public class FornecedorController {
+	private Fornecedor fornecedor;
+	private FornecedorRepositorio repositorio;
+	
+	@Autowired
+	public FornecedorController(Fornecedor fornecedor, FornecedorRepositorio repositorio) {
+		super();
+		this.fornecedor = fornecedor;
+		this.repositorio = repositorio;
+	}
+	
+	@RequestMapping("fornecedor")
+	public String fornecedorForm() {
+		return "fornecedor";
+	}
 	
 }
--- trabLanchonete/src/main/java/org/trabLanchonete/lanchonete/controller/PedidoController.java
+++ trabLanchonete/src/main/java/org/trabLanchonete/lanchonete/controller/PedidoController.java
@@ -2,13 +2,13 @@ package org.trabLanchonete.lanchonete.controller;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.lanchonetemaven.modelo.Carrinho;
-import org.lanchonetemaven.repositorio.ProdutoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.trabLanchonete.lanchonete.model.Pedido;
+import org.trabLanchonete.lanchonete.repositorio.PedidoRepositorio;
 
 @Controller
 public class PedidoController {
@@ -17,36 +17,36 @@ public class PedidoController {
 	private Pedido pedido;
 	
 	@Autowired
-	public PedidoControle(PedidoRepositorio repositorio, Pedido pedido) {
+	public PedidoController(PedidoRepositorio repositorio, Pedido pedido) {
 		super();
 		this.repositorio = repositorio;
 		this.pedido = pedido;
 	}
 	
-	@RequestMapping("listarPedidosCliente")
-	public String getProdutosLanchonete(Model model) {
-		model.addAttribute("pedido", repositorio.getPedido());
-		return "cardapio";
-	}
+//	@RequestMapping("listarPedidosCliente")
+//	public String getProdutosLanchonete(Model model) {
+//		model.addAttribute("pedido", repositorio.getPedido());
+//		return "cardapio";
+//	}
 	
-	@RequestMapping("listarPedidos")
-	public String getPedidos(Model model) {
-		model.addAttribute("pedido", carrinho.getPedidos());
-		return "pedido";
-	}
-	
-	@RequestMapping("adicionarPedido")
-	public String adicionarPedido(int codigo) {
-		carrinho.adicionar(repositorio.getPedido(codigo));
-		return "redirect:listarPedidos";
-	}
-	
-	@RequestMapping("removerPedido")
-	public String removerPedido(int codigo) {
-		carrinho.removerPedido(codigo);
-		return "redirect:listarPedidos";
-	}
-	
+//	@RequestMapping("listarPedidos")
+//	public String getPedidos(Model model) {
+//		model.addAttribute("pedido", carrinho.getPedidos());
+//		return "pedido";
+//	}
+//	
+//	@RequestMapping("adicionarPedido")
+//	public String adicionarPedido(int codigo) {
+//		carrinho.adicionar(repositorio.getPedido(codigo));
+//		return "redirect:listarPedidos";
+//	}
+//	
+//	@RequestMapping("removerPedido")
+//	public String removerPedido(int codigo) {
+//		carrinho.removerPedido(codigo);
+//		return "redirect:listarPedidos";
+//	}
+//	
 	
 }
 
